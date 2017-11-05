f56e503537f52985555b0e2df6587515b63468bd
Diego Cepeda <cepedadmc@gmail.com>
2017-11-04 20:36:38 -0400

    pls keypress

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,14 +3,16 @@ import java.awt.*;
 import java.awt.event.*;
 
 @SuppressWarnings("serial")
-public class GamePanel extends JPanel implements ActionListener{
+public class GamePanel extends JPanel {
 	
 	public GamePanel(Color bg) {
 		setBackground(bg);
 		Timer ptimer = new Timer(10, null);
+		ptimer.start();
 	}
 	
 	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
 		int h = getHeight();
 		int w = getWidth();
 		PlayerSpace s1 = new PlayerSpace(0, (int)(h * .33), (int)(h * .33), (int)(w * .33), Color.white);
@@ -26,11 +28,37 @@ public class GamePanel extends JPanel implements ActionListener{
 		Paddle p2 = new Paddle((s2w + s2.getcx()) - (int)(s2w * .1), (int)(s2w * .05) + s2.getcy(), (int)(s2h * .2), (int)(s2w * .02), Color.black);
 		p1.draw(g);
 		p2.draw(g);
-
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
 		
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keyCode = e.getKeyCode();
+				if (keyCode == KeyEvent.VK_UP) {
+					s2.move(0, -1);
+					repaint();
+				}
+				if (keyCode == KeyEvent.VK_DOWN) {
+					s2.move(0, 1);
+					repaint();
+				}
+				if (keyCode == KeyEvent.VK_LEFT) {
+					s2.move(-1, 0);
+					repaint();
+				}
+				if (keyCode == KeyEvent.VK_RIGHT) {
+					s2.move(1, 0);
+					repaint();
+				}
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+			}
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+			}	
+		});
 	}
+	
 }
